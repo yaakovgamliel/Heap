6c9d13cfed71c9724bfba795079944601dfa37dd
James Harton <jamesotron@gmail.com>
2012-11-21 22:28:49 +1300

    Fix bug in current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def current_user
-    @current_user ||= User.where(_id: session[:user_id]).first
+    @current_user ||= session[:user_id] && User.find(session[:user_id])
   end
 
   def signed_in?
-    !!@current_user
+    !!current_user
   end
 
   helper_method :current_user, :signed_in?
