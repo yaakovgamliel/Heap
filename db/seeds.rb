16e65d8b748b5492db1a18a49ea1ff8281bec452
James Harton <jamesotron@gmail.com>
2012-11-29 17:01:24 +1300

    Make banner styles more consistent and responsive. Should look great on mobile and PC.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,6 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 heap_user = User.create name: 'Heap Team', nickname: 'heap', location: 'The Cloud', description: 'The hard working team of monkeys behind [logheap.io](http://logheap.io/).', extra_url: 'http://logheap.io'
 
-heap_log = heap_user.create_heaplog name: 'Heap', description: 'Yet another blog. Simple.', short_name: 'heap'
+heap_log = heap_user.create_heaplog name: 'Heap', description: 'Yet another blog. Simple.', short_name: 'heap', banner_url: '/assets/logs1.jpg'
 
 
