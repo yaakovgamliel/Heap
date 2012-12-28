e8bbec20bc36f7ef9ec26597b35066999aeb863d
James Coglan <jcoglan@gmail.com>
2012-12-28 20:51:12 +0000

    Make cukes pass on Chrome using Terminus.

--- features/step_definitions/navigation_steps.rb
+++ features/step_definitions/navigation_steps.rb
@@ -1,11 +1,6 @@
 require 'uri'
 
 When /^I visit '(.+)'$/ do |uri|
-  unless uri =~ /:\/\//
-    u = URI.parse(root_url)
-    u.path = uri
-    uri = u.to_s
-  end
   visit uri
 end
 
@@ -16,5 +11,5 @@ Then /^I am redirected to '(.+)'$/ do |path|
 end
 
 When 'I go to the user\'s page' do
-  step "I visit '#{user_summary_url @user}'"
+  step "I visit '#{user_summary_path @user}'"
 end
